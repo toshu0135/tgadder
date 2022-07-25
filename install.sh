464f75d7d9b2fe551f3e302f9828c28d45b2e7b3
toshu0135 <87758363+toshu0135@users.noreply.github.com>
2022-07-25 14:32:17 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update && apt upgrade
+apt install python
+python -m pip install --upgrade pip
+apt install python git
+apt install -y git python
+pip install telethon
+pip install requests
+pip install colorama
+pip install pyrogram
+pip install tgcrypto
+echo "[+] installations Done"
